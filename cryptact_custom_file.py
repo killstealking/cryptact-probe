96c03ac20009bc976d15cd0a8335ba6f15561c90
Keisuke Iwamura <i.ksk0601@gmail.com>
2022-09-12 19:02:31 +0900

    wip

--- cryptact_custom_file.py
+++ cryptact_custom_file.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 from datetime import datetime
 from typing import Literal, Optional
 from caaj import Caaj
+import decimal
 
 Fields = Literal[
     "TimeStamp",
@@ -91,6 +92,30 @@ class CryptactRepository:
         """
         複数からなるcaajをcryptact形式にする
         """
+        get_caaj = None
+        lose_caaj = None
+        for caaj in caaj_list:
+            if caaj["type"] == "get":
+                get_caaj = caaj
+                continue
+            if caaj["type"] == "lose":
+                lose_caaj = caaj
+                continue
+        if get_caaj and lose_caaj:
+            decimal.getcontext().prec = 10
+            # https://support.cryptact.com/hc/ja/articles/360002571312-%E3%82%AB%E3%82%B9%E3%82%BF%E3%83%A0%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB%E3%81%AE%E4%BD%9C%E6%88%90%E6%96%B9%E6%B3%95-%E3%82%AB%E3%82%B9%E3%82%BF%E3%83%A0%E5%8F%96%E5%BC%95-#menu216
+            cryptact_format: CryptactCustomFile = {
+            "TimeStamp": get_caaj["executed_at"],
+            "Action": "Buy",
+            "Source": self._create_source_from_caaj(caaj=get_caaj),
+            "Base": self._convert_base_from_uti(uti=get_caaj["uti"]),
+            "Volume": get_caaj["amount"],
+            "Price": decimal.Decimal(lose_caaj["amount"])/decimal.Decimal(get_caaj["amount"]),
+            "Counter": self._convert_base_from_uti(uti=lose_caaj["uti"]),
+            "Fee": 0,
+            "FeeCry": "JPY",
+            "Comment": get_caaj["comment"]
+            }
         return None
 
     def create_cryptact_custom_files(self) -> None:
